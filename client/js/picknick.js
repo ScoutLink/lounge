80cf152196336ca066bcfd8a41a3f14fb0c60e7a
tea23 <joe@warhaggis.com>
2017-10-15 00:28:56 +0100

    Check for erroneous nicks

--- client/js/picknick.js
+++ client/js/picknick.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
-    $('.btn').attr('disabled',true);
-    $('#connect\\:nick').keyup(function(){
-        if($(this).val().length !=0){
-            $('.btn').attr('disabled', false); $('.picknick').css('display', 'none'); }
-        else
-            $('.btn').attr('disabled',true);
+$('.btn').attr('disabled',true);
+$('.picknick2').css('visibility', 'hidden');
+$('.picknick3').css('visibility', 'hidden');
+	
+	$('#connect\\:nick').keyup(function(){
+
+		var nicknumber = new RegExp("^[0-9]");
+
+		if($(this).val().length !=0){
+			$('.btn').attr('disabled', false); 
+			$('.picknick').css('visibility', 'hidden'); }
+		else {
+			$('.btn').attr('disabled',true);
+			$('.picknick').css('visibility', 'visible');
+
+		}
+
+		if ($("#connect\\:nick").val().match(nicknumber)) {
+
+			$('.picknick2').css('visibility', 'visible');
+			$('.btn').attr('disabled',true)
+
+		} else {
+
+			$('.picknick2').css('visibility', 'hidden');
+
+		}
+
     })
+
 });
