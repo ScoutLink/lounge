0f02a7ccafe02666ecfddc061174eca1ad4d1491
Ben Layton <me@benlayton.uk>
2017-10-20 16:52:26 +0100

    Tidy up picknick

--- client/js/picknick.js
+++ client/js/picknick.js
@@ -1,45 +1,33 @@
-$(document).ready(function(){
-$('.btn').attr('disabled',true);
-$('.picknick2').css('visibility', 'hidden');
-$('.picknick3').css('visibility', 'hidden');
+$(document).ready(function () {
+	$('.btn').attr('disabled',true); // TODO: Disabled attribute on form
+	$('.picknick2').css('visibility', 'hidden'); // TODO: Move to stylesheet
+	$('.picknick3').css('visibility', 'hidden'); // TODO: Move to stylesheet
 
-	$('#connect\\:nick').keyup(function(){
-
+	$('#connect\\:nick').keyup(function () {
 		var nicknumber = new RegExp("^[0-9]");
 		var spaces = new RegExp("\\\s|\\:|\\@|\\!|\\/|\\.");
 
-		if($(this).val().length !=0){
-			$('.btn').attr('disabled', false); 
-			$('.picknick').css('visibility', 'hidden'); }
-		else {
-			$('.btn').attr('disabled',true);
-			$('.picknick').css('visibility', 'visible');
-
-		}
-
-		if ($("#connect\\:nick").val().match(nicknumber)) {
-
-			$('.picknick2').css('visibility', 'visible');
-			$('.btn').attr('disabled',true)
-
+		// TODO: Refactor
+		if ($(this).val().length != 0) {
+			$('.btn').attr('disabled', false);
+			$('.picknick').css('visibility', 'hidden');
 		} else {
-
-			$('.picknick2').css('visibility', 'hidden');
-
+			$('.btn').attr('disabled', true);
+			$('.picknick').css('visibility', 'visible');
 		}
 
-                if ($("#connect\\:nick").val().match(spaces)) {
+		if ($(this).val().match(nicknumber)) {
+			$('.picknick2').css('visibility', 'visible');
+			$('.btn').attr('disabled', true)
+		} else {
+			$('.picknick2').css('visibility', 'hidden');
+		}
 
-                        $('.picknick3').css('visibility', 'visible');
-                        $('.btn').attr('disabled',true)
-
-                } else {
-
-                        $('.picknick3').css('visibility', 'hidden');
-
-                }
-
-
-    })
-
+        if ($(this).val().match(spaces)) {
+            $('.picknick3').css('visibility', 'visible');
+            $('.btn').attr('disabled', true)
+        } else {
+            $('.picknick3').css('visibility', 'hidden');
+        }
+    });
 });
